1ce4533c22ca1d13e787d7791cce6f8505cae1b5
lilyang1989 <845630236@qq.com>
2022-05-14 22:19:44 +0800

    Create hello.sol

---
+++ SolidityByExample/hello.sol
@@ -0,0 +1,23 @@
+pragma solidity ^0.8.13;
+ contract Hello{
+     string public greet="hello";
+ }
+ contract Counter {
+    uint public count;
+
+    // Function to get the current count
+    function get() public view returns (uint) {
+        return count;
+    }
+    //可见性修饰符、状态修饰符、再return自定义
+    // Function to increment count by 1
+    function inc() public {
+        count += 1;
+    }
+
+    // Function to decrement count by 1
+    function dec() public {
+        // This function will fail if count = 0
+        count -= 1;
+    }
+}
