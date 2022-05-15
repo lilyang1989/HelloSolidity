18b10637e941ee274095c6b5950ed84716218d18
lilyang1989 <845630236@qq.com>
2022-05-15 23:55:31 +0800

    Update hello.sol

--- SolidityByExample/hello.sol
+++ SolidityByExample/hello.sol
@@ -14,8 +14,6 @@ pragma solidity ^0.8.13;
     function inc() public {
         count += 1;
     }
-
-
     // Function to decrement count by 1
     function dec() public {
         // This function will fail if count = 0
