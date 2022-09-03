d06b7f69a04df92b926cab32cc2540fc3c56c17b
lilyang1989 <845630236@qq.com>
2022-09-03 08:09:22 +0800

    Create inheritance.sol

---
+++ SolidityByExample/inheritance.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+// virtual 代表可以被子合约覆盖
+//override代表可以覆盖父合约
+//pure修饰的函数既不能改也不能读状态变量 否则编译无法通过
+//view只能读状态变量但不能改
+//super关键字将调用最近的父合约函数
+contract A{
+    function foo() public pure virtual returns(string memory){
+        return "A";
+    }
+}
+
+contract B is A{
+    function foo() public pure virtual override returns (string memory){
+        return "B";
+    }
+}
+
+contract C is A{
+    function foo() public pure virtual override returns (string memory){
+        return "C";
+    }
+}
+
+contract D is B,C{
+    function foo() public  pure override(B,C) returns(string memory){
+        return super.foo(); //调用C
+}
+}
+
+contract E is C,B{
+    function foo() public pure override(C,B) returns(string memory){
+        return super.foo(); //调用B
+    }
+}
+
+contract F is A,B{
+    function foo() public pure override(A,B) returns(string memory){
+        return super.foo();
+    }
+}
